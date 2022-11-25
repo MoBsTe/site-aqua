b5dfded8bb65b986efee1059e7cb6f8320a6bcbd
MoBsTe <valery.lobach@gail.com>
2022-11-25 21:01:54 +0100

    add js on site

---
+++ script.js
@@ -0,0 +1,37 @@
+
+let animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+    function animOnScroll(params) {
+        for (let index = 0; index < animItems.length; index++) {
+            let animItem = animItems[index];
+            let animItemHeight = animItem.offsetHeight;
+            let animItemOffset = offset(animItem).top;
+            let animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('_active');
+            } else {
+                animItem.classList.remove('_active');
+            }
+
+        }
+    }
+
+    function offset(el) {
+        let rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+
+    }
+    animOnScroll();
+}
